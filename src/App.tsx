b99cf2d0aa422a5e8f0926fc2f9f85a0b9c4cb45
LuisMondragonB <124083000+LuisMondragonB@users.noreply.github.com>
2025-08-24 15:28:28 -0600

    Arreglar problema de enrutamiento /admin

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import HomePage from "./pages/HomePage";
 import MembershipsPage from "./pages/MembershipsPage";
 import AdminPage from "./pages/AdminPage";
 import Navigation from "./components/Layout/Navigation";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  
+  // Debug: mostrar la ruta actual
+  console.log('Current route:', location.pathname);
+
   return (
     <>
-      <Navigation />
+      {/* Solo mostrar navegación en páginas públicas */}
+      {location.pathname !== '/admin' && <Navigation />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/membresias" element={<MembershipsPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        {/* Ruta catch-all para debugging */}
+        <Route path="*" element={<div>Ruta no encontrada: {location.pathname}</div>} />
       </Routes>
     </>
   );
